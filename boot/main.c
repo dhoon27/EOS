bd7aed66a66b9493e4f3949ca7207f1c303bb11b
dhoon27 <ijdmc2336@gmail.com>
2022-12-26 12:30:10 +0900

    EOS: Context Switching
     This Commit is for Context Switching.
    
    Signed-off-by: dhoon27 <ijdmc2336@gmail.com>

--- boot/main.c
+++ boot/main.c
@@ -8,58 +8,131 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-static void hw_init(void);
+#include "Kernel.h"
 
-static void printf_test(void);
+static void Hw_init(void);
+static void Kernel_init(void);
+
+static void Printf_test(void);
 static void Timer_test(void);
 
-int main()
+void User_task0(void);
+void User_task1(void);
+void User_task2(void);
+
+void main(void)
 {
-    hw_init();
+    Hw_init();
 
-    int i = 0;
-    while(i--) {
-        uint8_t ch = Hal_uart_get_char();
-        Hal_uart_put_char(ch);
-
+    uint32_t i = 100;
+    while(i--)
+    {
+        Hal_uart_put_char('N');
     }
     Hal_uart_put_char('\n');
 
-    putstr("Hello World\n");
-    
-    printf_test();
-    Timer_test();
-    while(true);
+    putstr("Hello World!\n");
 
-    return 0;
+    Printf_test();
+    Timer_test();
+
+    Kernel_init();
+
+    while(true);
 }
 
-static void hw_init()
+static void Hw_init(void)
 {
     Hal_interrupt_init();
     Hal_uart_init();
     Hal_timer_init();
 }
 
-static void printf_test()
+
+static void Kernel_init(void)
 {
-    char* str = "printf test";
+    uint32_t taskId;
+
+    Kernel_task_init();
+
+    taskId = Kernel_task_create(User_task0);
+    if (NOT_ENOUGH_TASK_NUM == taskId)
+    {
+        putstr("Task0 creation fail\n");
+    }
+
+    taskId = Kernel_task_create(User_task1);
+    if (NOT_ENOUGH_TASK_NUM == taskId)
+    {
+        putstr("Task1 creation fail\n");
+    }
+
+    taskId = Kernel_task_create(User_task2);
+    if (NOT_ENOUGH_TASK_NUM == taskId)
+    {
+        putstr("Task2 creation fail\n");
+    }
+
+    Kernel_start();
+}
+
+static void Printf_test(void)
+{
+    char* str = "printf pointer test";
     char* nullptr = 0;
     uint32_t i = 5;
     uint32_t* sysctrl0 = (uint32_t*)0x10001000;
-    
-    debug_printf("%s\n", "hello printf");
-    debug_printf("str pointer : %s\n", str);
-    debug_printf("%s is nullptr, %u number\n",nullptr, 10);
-    debug_printf("dec = %u, hex = %x\n",0xff, 0xAA);
+
+    debug_printf("%s\n", "Hello printf");
+    debug_printf("output string pointer: %s\n", str);
+    debug_printf("%s is null pointer, %u number\n", nullptr, 10);
+    debug_printf("%u = 5\n", i);
+    debug_printf("dec=%u hex=%x\n", 0xff, 0xff);
+    debug_printf("print zero %u\n", 0);
     debug_printf("SYSCTRL0 %x\n", *sysctrl0);
 }
 
 static void Timer_test(void)
 {
-    while(true)
+    for(uint32_t i = 0; i < 5 ; i++)
     {
         debug_printf("current count : %u\n", Hal_timer_get_1ms_counter());
         delay(1000);
+    }
+}
+
+void User_task0(void)
+{
+    uint32_t local = 0;
+
+
+    while(true)
+    {
+        debug_printf("User Task #0 SP=0x%x\n", &local);
+        Kernel_yield();
+    }
+}
+
+void User_task1(void)
+{
+    uint32_t local = 0;
+
+
+    while(true)
+    {
+        debug_printf("User Task #1 SP=0x%x\n", &local);
+        Kernel_yield();
+    }
+}
+
+void User_task2(void)
+{
+    uint32_t local = 0;
+
+
+    while(true)
+    {
+        debug_printf("User Task #2 SP=0x%x\n", &local);
+        Kernel_yield();
     }
 }
